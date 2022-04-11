7899422709f60f27c29cf6dcc551c2b69faa990b
Nazirul <20ftt1440@student.pb.edu.bn>
2022-04-11 21:13:42 +0800

    20ftt1440 - index.css added

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <p class="text-3xl">Hello World!</p>
 </template>
 
 <style>
